f68cdcdf37531dacfbe12a74d2937c257b73d591
16CO118 <16co118@example.com>
2018-04-02 12:12:15 +0530

    Added Comments to checkcorrect.m

--- checkcorrect.m
+++ checkcorrect.m
@@ -1,11 +1,14 @@
 function true = checkcorrect(value)
-%Function for checking if a mapping is correct
+%Function for checking if a mapping is correct as per the constraints
 
+%The truth value is stored in flag
 flag = 0;
 
 counter = 1;
+%The first element is taken into check
 check = value(1);
 
+%Whenever three consecutive occurences of an element occurs we get an error
 for i = 2:8
     if(value(i) == check)
         counter = counter + 1;
