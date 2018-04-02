6c606b85748d1ec1d9e89b2b314f772041084444
16CO208 <16co208@example.com>
2018-04-02 12:54:47 +0530

    Upload main.m

---
+++ main.m
@@ -0,0 +1,9 @@
+% Script File for 12B8T Encoding Scheme
+
+%binlist stores the list of 12 bit binary numbers
+binlist = binarylist(12);
+%terlist store the list of all 8 bit binary numbers
+terlist = ternarylist(8);
+
+%maplis has the unique mapped list
+maplis = mapping(binlist,terlist);
