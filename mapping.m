40567a3c7465591c872834cd7c169b1041cb71db
16CO208 <16co208@example.com>
2018-04-02 12:10:32 +0530

    Added Comments to mapping.m

--- mapping.m
+++ mapping.m
@@ -1,11 +1,18 @@
 function mappedlist = mapping(binlist,terlist)
 %Function for unique mapping from binary list to ternary list
 
+%binlen and terlen store the size of the lists
 binlen = size(binlist);
 binlen = binlen(2);
 terlen = size(terlist);
 terlen = terlen(2);
 
+%The following lines initialize a second row for mapping
+binlist{2,1} = [];
+terlist{2,1} = [];
+
+%In this for loop each element of the binlist is assigned a unique mapping in
+%terlist
 for i = 1:binlen
     num = randi([1,terlen]);
     if(terlist{2,num} == -1)
@@ -25,6 +32,7 @@ for i = 1:binlen
     end        
 end
 
+%Returns The Mapped List
 mappedlist = binlist;
 
 end
