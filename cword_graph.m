4bda622c97d70753e4b7805935a34494e17212be
16CO118 <16co118@example.com>
2018-04-15 14:09:59 +0530

    Update cword_graph.m to include multiple groups

--- cword_graph.m
+++ cword_graph.m
@@ -3,15 +3,26 @@ function flag = cword_graph(proclist)
 %Graphing by taking dataword as input
 
 dword = input('Enter the dataword\n','s');
-display(dword);
-%Split the dataword in groups of 12
-len = length(proclist);
+%New Array
+p = {};
+%Findig no of groups to take
+n = length(dword)/12;
+for j = 1:n
+    dword1 = dword(12*(j-1)+1:12*j);
+    %Split the dataword in groups of 12
+    len = length(proclist);
 
-for i = 1:len
-    if(proclist{1,i} == dword)
-        wave(proclist{2,i})
+    for i = 1:len
+        if(proclist{1,i} == dword1)
+            p = [p,proclist{2,i}];
+            %wave(proclist{2,i})
+        end
     end
-end    
+end
+newStr = cell2mat(p);
+wave(newStr,n*8);
 flag = 1; 
+
+
 
 end
